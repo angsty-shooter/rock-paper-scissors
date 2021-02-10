a3d93fd91193eaff9f4e20f254e27efd91d8f902
Ashley Herrera <ashleyherrera131@gmail.com>
2021-02-10 16:58:06 -0700

    final messages

--- script.js
+++ script.js
@@ -21,47 +21,57 @@ function play(pChoice){
     console.log("You chose " + pChoice)
     random()
     if(pChoice == rock){
+    document.getElementById("player").innerText = "You chose rock"
         if(cpuChoice == rock){
             document.getElementById("cpu").innerText = "Computer chose rock"
             document.getElementById("status").innerText = "Tie"
             return "Tie"
         }
         else if(cpuChoice == paper){
+            document.getElementById("cpu").innerText = "Computer chose paper"
             document.getElementById("status").innerText = "You Lose"
             return "You Lose"
         }
         else{//scissors
+            document.getElementById("cpu").innerText = "Computer chose scissors"
             document.getElementById("status").innerText = "You Win"
             return "You Win"
         }
 
     }
     if(pChoice == paper){
+    document.getElementById("player").innerText = "You chose paper"
         if(cpuChoice == rock){
+            document.getElementById("cpu").innerText = "Computer chose rock"
             document.getElementById("status").innerText = "You Win"
             return "You Win"
         }
-
         else if(cpuChoice == paper){
+            document.getElementById("cpu").innerText = "Computer chose paper"
             document.getElementById("status").innerText = "Tie"
             return "Tie"
         }
         else{//scissors
+            document.getElementById("cpu").innerText = "Computer chose scissors"
             document.getElementById("status").innerText = "You Lose"
             return "You Lose"
         }
 
     }
     if(pChoice == scissors){
+    document.getElementById("player").innerText = "You chose Scissors"
         if(cpuChoice == rock){
+            document.getElementById("cpu").innerText = "Computer chose rock"
             document.getElementById("status").innerText = "You Lose"
             return "You Lose"
         }
         else if(cpuChoice == paper){
+            document.getElementById("cpu").innerText = "Computer chose paper"
             document.getElementById("status").innerText = "You Win"
             return "You Win"
         }
         else{//scissors
+            document.getElementById("cpu").innerText = "Computer chose scissors"
             document.getElementById("status").innerText = "Tie"
             return "Tie"
         }
